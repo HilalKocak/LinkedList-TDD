5aa201c1edb48b7a913c59976d412c1cde4b0caa
Hilal38 <hhilalkocak@gmail.com>
2024-02-27 08:37:19 +0300

    Organize test files

--- LinkedList.test.js
+++ LinkedList.test.js
@@ -21,25 +21,28 @@ describe('getByIndex', ()=> {
             expect(ll.getByIndex(-1)).toBeNull()
         })
     })
+
+    describe('with index greater than list length', ()=> {
+        test('it returns null', ()=> {
+            const ll = new LinkedList.fromValues(10, 20)
+            expect(ll.getByIndex(5)).toBeNull()
+        })
+    })
+    
+    describe('with index 0', ()=> {
+        test('it returns the head', ()=> {
+            const ll = LinkedList.fromValues(10,20)
+            expect(ll.getByIndex(0).value).toBe(10)
+        })
+    })
+    
+    describe('with index in the middle', ()=> {
+        test('it returns the element at that index', ()=> {
+            const ll = LinkedList.fromValues(10, 20, 30, 40)
+            expect(ll.getByIndex(2).value).toBe(30)
+        })
+    })
+
+
 })
 
-describe('with index greater than list length', ()=> {
-    test('it returns null', ()=> {
-        const ll = new LinkedList.fromValues(10, 20)
-        expect(ll.getByIndex(5)).toBeNull()
-    })
-})
-
-describe('with index 0', ()=> {
-    test('it returns the head', ()=> {
-        const ll = LinkedList.fromValues(10,20)
-        expect(ll.getByIndex(0).value).toBe(10)
-    })
-})
-
-describe('with index in the middle', ()=> {
-    test('it returns the element at that index', ()=> {
-        const ll = LinkedList.fromValues(10, 20, 30, 40)
-        expect(ll.getByIndex(2).value).toBe(30)
-    })
-})
